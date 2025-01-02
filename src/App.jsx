00d70a2268e54588758349015b589c7b499c411a
Arijit Pal <arijitpalguskara@gmail.com>
2025-01-02 09:50:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from './Main/Hero'
+import Pricing from './Main/Pricing'
+import Waitlist from './Main/Waitlist'
+import Lamp from './Main/Lamp'
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Pricing />
+      <Waitlist />
+     <Lamp />
+    </div>
+  )
+}
+
+export default App
